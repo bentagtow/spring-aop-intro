7291183aa21c2a360108b9d6eff61d4b412f5b60
bentagtow <94013701+bentagtow@users.noreply.github.com>
2021-12-01 16:50:40 -0600

    renames beforeaccess class

---
+++ src/main/java/com/in28minutes/spring/aop/springaop/aspect/UserAccessAspect.java
@@ -0,0 +1,25 @@
+package com.in28minutes.spring.aop.springaop.aspect;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
+
+//AOP
+//Configuration
+@Aspect
+@Configuration
+public class UserAccessAspect {
+
+    //what kind of method calls to intercept
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Before("execution(* com.in28minutes.spring.aop.springaop.business.*.*(..))")
+    public void before(JoinPoint joinPoint){
+        logger.info("check for user access");
+        logger.info("Intercepted Method Calls - {}", joinPoint);
+    }
+}
