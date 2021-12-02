df075b0d16aa27cb940a473608fd34b352585780
bentagtow <94013701+bentagtow@users.noreply.github.com>
2021-12-02 09:23:08 -0600

    adds custom annotations - completes section 9

--- src/main/java/com/in28minutes/spring/aop/springaop/aspect/CommonJoinPointConfig.java
+++ src/main/java/com/in28minutes/spring/aop/springaop/aspect/CommonJoinPointConfig.java
@@ -10,4 +10,17 @@ public class CommonJoinPointConfig {
     @Pointcut("execution(* com.in28minutes.spring.aop.springaop.business.*.*(..))")
     public void businessLayerExecution(){};
 
+    @Pointcut("com.in28minutes.spring.aop.springaop.aspect.CommonJoinPointConfig.dataLayerExecution() && com.in28minutes.spring.aop.springaop.aspect.CommonJoinPointConfig.businessLayerExecution()")
+    public void allLayerExecution(){}
+
+    //would intercept any bean containing dao
+    @Pointcut("bean(*dau*)")
+    public void beanContainingDao(){}
+
+    @Pointcut("within(*com.in28minutes.spring.aop.springaop.data..*)")
+    public void dataLayerExecutionWithWithin(){}
+
+    @Pointcut("@annotation(com.in28minutes.spring.aop.springaop.aspect.TrackTime)")
+    public void trackTimeAnnotation(){}
+
 }
